29120af9b867cb48af93c6311ff7886a604c7000
Javier See <70385670+chichao89@users.noreply.github.com>
2025-02-06 07:50:56 +0800

    6th  commit

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "chichaos3.bucket"  # Replace with your actual S3 bucket name
+    bucket = "chichaos3.sctp-sandbox.com"  # Replace with your actual S3 bucket name
     key    = "terraform/state/terraform.tfstate"   # Path where the state file will be stored in the S3 bucket
     region = "ap-southeast-1"              # AWS region where the S3 bucket is located
   }
@@ -12,7 +12,7 @@ provider "aws" {
 
 # Create the S3 bucket
 resource "aws_s3_bucket" "static_bucket" {
-  bucket        = "chichaos3.bucket"  # Replace with your desired bucket name
+  bucket        = "chichaos3.sctp-sandbox.com"  # Replace with your desired bucket name
   force_destroy = true
 
   tags = {
